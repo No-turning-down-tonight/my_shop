7dbb1a31e53a871763f592122fe60d795f6bf79f
王二狗 <wangergou@163.com>
2019-12-20 11:11:02 +0800

    登陆页面的面板

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import ElementUI from 'element-ui';
+import router from './router/index.js'
+import './assets/css/global.css'
 import './plugins/element.js'
-import 'element-ui/lib/theme-chalk/index.css';
-
-Vue.use(ElementUI);
+import './assets/fonts/iconfont.css'
 Vue.config.productionTip = false
 
 new Vue({
